1988dcfe753ed423d8ec863e27aebcd3ee5234b3
Sakshi <sakshisrivastava413@gmail.com>
2018-07-28 14:19:31 +0530

    created mock data

--- app.js
+++ app.js
@@ -3,11 +3,34 @@ const jwt = require('jsonwebtoken');
 
 const app = express();
 
-app.get('/', function (req, res) {
+app.get('/api', function (req, res) {
   res.json({
     message: "welcome to the api"
   });
 });
+
+app.post('/api/post', function (req, res) {
+  res.json({
+    message: "Post created ..."
+  });
+});
+
+app.post('/api/login', function (req, res) {
+
+  const user = {
+    id: 1,
+    username: "sakshi",
+    email: "sakshi@jwt.com"
+  }
+
+  jwt.sign({ user }, 'secretkey', (err, token) => {
+    res.json({
+      token
+    });
+  });
+});
+
+
 
 app.listen(process.env.port || 5000,
   () => { console.log("server started on port 5000") })
